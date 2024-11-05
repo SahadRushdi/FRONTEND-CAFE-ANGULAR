22d7525fbb0fb45cdfdd60420bc96b44f215184c
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-11-05 10:54:30 +0530

    Implemented jwt Decode.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,12 @@ import { SharedModule } from './shared/shared.module';
 import { FullComponent } from './layouts/full/full.component';
 import { AppHeaderComponent } from './layouts/full/header/header.component';
 import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
-import { HttpClientModule } from '@angular/common/http';
-// import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+// import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { SignupComponent } from './signup/signup.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { LoginComponent } from './login/login.component';
-// import { TokenInterceptorInterceptor } from './services/token-interceptor.interceptor';
+import { TokenInterceptorInterceptor } from './services/token-interceptor.interceptor';
 @NgModule({
   declarations: [	
     AppComponent,
@@ -42,8 +42,8 @@ import { LoginComponent } from './login/login.component';
     SharedModule,
     HttpClientModule
   ],
-  // providers: [HttpClientModule,{provide:HTTP_INTERCEPTORS,useClass:TokenInterceptorInterceptor,multi:true}],
-  providers: [],
+  providers: [HttpClientModule,{provide:HTTP_INTERCEPTORS,useClass:TokenInterceptorInterceptor,multi:true}],
+  // providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
