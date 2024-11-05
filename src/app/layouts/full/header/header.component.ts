19c89ee93b1f12e9f01d6efb4d185c2359ba164e
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2024-11-05 13:35:01 +0530

    Made Change Password and Logout Button

--- src/app/layouts/full/header/header.component.ts
+++ src/app/layouts/full/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { ChangePasswordComponent } from 'src/app/material-component/dialog/change-password/change-password.component';
+import { ConfirmationComponent } from 'src/app/material-component/dialog/confirmation/confirmation.component';
 
 @Component({
   selector: 'app-header',
@@ -9,9 +11,27 @@ import { Router } from '@angular/router';
 })
 export class AppHeaderComponent {
   role: any;
-  constructor(private router: Router,
-    private dialog: MatDialog) {
 
-  }
+  constructor(private router: Router,
+    private dialog: MatDialog) {}
+
+    logout() {
+      const dialogCofig = new MatDialogConfig();
+      dialogCofig.data = {
+        message: 'Logout'
+      };
+      const dialogRef = this.dialog.open(ConfirmationComponent, dialogCofig);
+      const sub = dialogRef.componentInstance.onEmitStatusChange.subscribe((user) => {
+        dialogRef.close();
+        localStorage.clear();
+        this.router.navigate(['/']);
+      })
+    }
+
+    changePassword() {
+      const dialogCofig = new MatDialogConfig();
+      dialogCofig.width = "550px";
+      this.dialog.open(ChangePasswordComponent,dialogCofig);
+    }
 
 }
